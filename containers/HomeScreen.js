db0f81636b2d3c323b9b02157196738ed9149f98
Gérold <gerold380@hotmail.com>
2022-08-11 17:34:41 +0200

    modified:   containers/HomeScreen.js

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -13,22 +13,6 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { useSafeAreaFrame } from "react-native-safe-area-context";
-//données
-
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-];
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -57,8 +41,6 @@ export default function HomeScreen() {
         console.log(error.message);
       }
     }, [lease]);
-
-    // return <View></View>;
   };
   showlease();
   return leaseIsLoading === true ? (
